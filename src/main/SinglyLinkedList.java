55992997fbf23c4f9e4e2e5b641b6bd37b9c09f8
Priyabanga <priya.banga_ccv17@gla.ac.in>
2019-02-26 10:44:41 -0800

    init commit

---
+++ src/main/SinglyLinkedList.java
@@ -0,0 +1,16 @@
+package main;
+
+public class SinglyLinkedList {
+    Node head;
+
+    private class Node
+    {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+    }
+}
