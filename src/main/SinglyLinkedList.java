7bf6843409789a291da38f5803bedb1ce7646b20
Priyabanga <priya.banga_ccv17@gla.ac.in>
2019-02-26 10:58:58 -0800

    3 node added

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -3,8 +3,7 @@ package main;
 public class SinglyLinkedList {
     Node head;
 
-    private class Node
-    {
+    private static class Node {
         private int data;
         private Node next;
 
@@ -12,5 +11,27 @@ public class SinglyLinkedList {
             this.data = data;
         }
 
+        public int getData() {
+            return data;
+        }
     }
-}
+        public void display() {
+            Node n = head;
+            while (n != null) {
+                System.out.print(n.getData()+" ");
+                n = n.next;
+            }
+        }
+
+
+        public static void main(String[] args) {
+            SinglyLinkedList l = new SinglyLinkedList();
+            l.head = new Node(1);
+            Node second = new Node(2);
+            Node third = new Node(3);
+            l.head.next = second;
+            second.next = third;
+            l.display();
+        }
+    }
+
